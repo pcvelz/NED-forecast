f1dce3695886053416a07751da9a7cff41bc528f
BravoNLD <155718843+BravoNLD@users.noreply.github.com>
2026-01-04 16:25:19 +0100

    Update __init__.py AFF

--- custom_components/ned_energy_forecast/__init__.py
+++ custom_components/ned_energy_forecast/__init__.py
@@ -9,7 +9,7 @@ from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
-from .coordinator import NEDDataUpdateCoordinator
+from .coordinator import NEDEnergyDataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -18,7 +18,7 @@ PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up NED Energy Forecast from a config entry."""
-    coordinator = NEDDataUpdateCoordinator(hass, entry)
+    coordinator = NEDEnergyDataUpdateCoordinator(hass, entry)
     
     await coordinator.async_config_entry_first_refresh()
     
